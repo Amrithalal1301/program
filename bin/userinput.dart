23fd03ffdd48840f7a5458ff1c47013e27fca8a3
AMRITHA LAL <lalamritha42@gmail.com>
2022-09-23 10:51:00 +0530

    commit

--- bin/userinput.dart
+++ bin/userinput.dart
@@ -3,13 +3,21 @@ import 'dart:io';
 
 void main(){
   //dart io ----standard input output
-  print("enter your name");
+  print("enter your name :");
   var name = stdin.readLineSync();
-  print ("enter your age");
+  print ("enter email :");
+  var email = stdin.readLineSync();
+  print ("enter address :");
+  var address = stdin.readLineSync();
+  print ("enter your age:");
   int age =int.parse(stdin.readLineSync()!);
   print("enter mark");
   var mark = double.parse(stdin.readLineSync()!);
+  print("-------------------");
   print('hello $name');
+  print('address : $address');
+  print('id : $email');
   print('your age  $age');
   print('your mark $mark ');
+  print("-------------------");
 }
